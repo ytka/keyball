b0b16cf16bcbae5e47984ec476687965dbde6948
ytka <yutakayy@gmail.com>
2024-07-25 22:35:53 +0900

    custom

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via-ytka/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via-ytka/config.h
@@ -27,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#    define RGBLIGHT_EFFECT_SNAKE
 //#    define RGBLIGHT_EFFECT_KNIGHT
 //#    define RGBLIGHT_EFFECT_CHRISTMAS
-#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+//#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
 //#    define RGBLIGHT_EFFECT_RGB_TEST
 //#    define RGBLIGHT_EFFECT_ALTERNATING
 //#    define RGBLIGHT_EFFECT_TWINKLE
@@ -37,3 +37,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 1
+
+
+#define OLED_TIMEOUT 30000 // ミリ秒
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+#define KEYBALL_CPI_DEFAULT 1100 // マウス速度 (default: 500)
+#define KEYBALL_SCROLL_DIV_DEFAULT 5 // スクロール速度 (default: 4)
+
+#define COMBO_ONLY_FROM_LAYER 0
