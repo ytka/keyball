ac29e704e1aa3fa61fcba9590ea47a5e88e1394f
ytka <yutakayy@gmail.com>
2024-08-19 15:00:14 +0900

    advanced tap dance

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via-ytka/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via-ytka/keymap.c
@@ -22,21 +22,36 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 #ifdef TAP_DANCE_ENABLE
+
+typedef struct {
+    bool is_press_action;
+    int state;
+} tap;
+
+enum {
+    SINGLE_TAP = 1,
+    SINGLE_HOLD = 2,
+    DOUBLE_TAP = 3,
+    DOUBLE_HOLD = 4,
+    DOUBLE_SINGLE_TAP = 5, //send two single taps
+    TRIPLE_TAP = 6,
+    TRIPLE_HOLD = 7
+  };
+
+int cur_dance (tap_dance_state_t *state);
+
+//for the x tap dance. Put it here so it can be used in any keymap
+void x_finished (tap_dance_state_t *state, void *user_data);
+void x_reset (tap_dance_state_t *state, void *user_data);
+
+
 // Tap Dance declarations
 enum {
     TD_Q_ESC,
     TD_RCMD_LANG2,
+//    TD_RCMD_LANG2,
     TD_Z_SFTZ,
     //TD_LB_RB,
-};
-
-// Tap Dance definitions
-tap_dance_action_t tap_dance_actions[] = {
-    // Tap once for Escape, twice for Caps Lockqlj
-    [TD_Q_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_Q, KC_ESCAPE),
-    [TD_RCMD_LANG2] = ACTION_TAP_DANCE_DOUBLE(KC_RCMD, KC_LNG2),
-    [TD_Z_SFTZ] = ACTION_TAP_DANCE_DOUBLE(KC_Z, S(KC_Z)),
-    //[TD_LB_RB] = ACTION_TAP_DANCE_DOUBLE(KC_LEFT_BRACKET, KC_RIGHT_BRACKET),
 };
 
 /*
@@ -47,6 +62,102 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // ...
 };
 */
+
+/* Return an integer that corresponds to what kind of tap dance should be executed.
+ *
+ * How to figure out tap dance state: interrupted and pressed.
+ *
+ * Interrupted: If the state of a dance dance is "interrupted", that means that another key has been hit
+ *  under the tapping term. This is typically indicitive that you are trying to "tap" the key.
+ *
+ * Pressed: Whether or not the key is still being pressed. If this value is true, that means the tapping term
+ *  has ended, but the key is still being pressed down. This generally means the key is being "held".
+ *
+ * One thing that is currenlty not possible with qmk software in regards to tap dance is to mimic the "permissive hold"
+ *  feature. In general, advanced tap dances do not work well if they are used with commonly typed letters.
+ *  For example "A". Tap dances are best used on non-letter keys that are not hit while typing letters.
+ *
+ * Good places to put an advanced tap dance:
+ *  z,q,x,j,k,v,b, any function key, home/end, comma, semi-colon
+ *
+ * Criteria for "good placement" of a tap dance key:
+ *  Not a key that is hit frequently in a sentence
+ *  Not a key that is used frequently to double tap, for example 'tab' is often double tapped in a terminal, or
+ *    in a web form. So 'tab' would be a poor choice for a tap dance.
+ *  Letters used in common words as a double. For example 'p' in 'pepper'. If a tap dance function existed on the
+ *    letter 'p', the word 'pepper' would be quite frustating to type.
+ *
+ * For the third point, there does exist the 'DOUBLE_SINGLE_TAP', however this is not fully tested
+ *
+ */
+int cur_dance (tap_dance_state_t *state) {
+  if (state->count == 1) {
+    if (state->interrupted || !state->pressed)  return SINGLE_TAP;
+    //key has not been interrupted, but they key is still held. Means you want to send a 'HOLD'.
+    else return SINGLE_HOLD;
+  }
+  else if (state->count == 2) {
+    /*
+     * DOUBLE_SINGLE_TAP is to distinguish between typing "pepper", and actually wanting a double tap
+     * action when hitting 'pp'. Suggested use case for this return value is when you want to send two
+     * keystrokes of the key, and not the 'double tap' action/macro.
+    */
+    if (state->interrupted) return DOUBLE_SINGLE_TAP;
+    else if (state->pressed) return DOUBLE_HOLD;
+    else return DOUBLE_TAP;
+  }
+  //Assumes no one is trying to type the same letter three times (at least not quickly).
+  //If your tap dance key is 'KC_W', and you want to type "www." quickly - then you will need to add
+  //an exception here to return a 'TRIPLE_SINGLE_TAP', and define that enum just like 'DOUBLE_SINGLE_TAP'
+  if (state->count == 3) {
+    if (state->interrupted || !state->pressed)  return TRIPLE_TAP;
+    else return TRIPLE_HOLD;
+  }\
+  else return 8; //magic number. At some point this method will expand to work for more presses
+}
+
+//instanalize an instance of 'tap' for the 'x' tap dance.
+static tap xtap_state = {
+  .is_press_action = true,
+  .state = 0
+};
+
+void x_finished (tap_dance_state_t *state, void *user_data) {
+  xtap_state.state = cur_dance(state);
+  switch (xtap_state.state) {
+    case SINGLE_TAP: register_code(KC_LNG2); break;
+    case SINGLE_HOLD: layer_on(5); break;
+    case DOUBLE_TAP: register_code(KC_LNG1); break;
+    case DOUBLE_HOLD: register_code(KC_RGUI); break;
+    case DOUBLE_SINGLE_TAP: register_code(KC_LNG2); break;
+//    case DOUBLE_SINGLE_TAP: register_code(KC_LNG2); unregister_code(KC_LNG2); register_code(KC_LNG2);
+    //Last case is for fast typing. Assuming your key is `f`:
+    //For example, when typing the word `buffer`, and you want to make sure that you send `ff` and not `Esc`.
+    //In order to type `ff` when typing fast, the next character will have to be hit within the `TAPPING_TERM`, which by default is 200ms.
+  }
+}
+
+void x_reset (tap_dance_state_t *state, void *user_data) {
+  switch (xtap_state.state) {
+    case SINGLE_TAP: unregister_code(KC_LNG2); break;
+    case SINGLE_HOLD: layer_off(5); break;
+    case DOUBLE_TAP: unregister_code(KC_LNG1); break;
+    case DOUBLE_HOLD: unregister_code(KC_RGUI);
+    case DOUBLE_SINGLE_TAP: unregister_code(KC_LNG2);
+  }
+  xtap_state.state = 0;
+}
+
+// Tap Dance definitions
+tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for Escape, twice for Caps Lockqlj
+    [TD_Q_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_Q, KC_ESCAPE),
+//    [TD_RCMD_LANG2] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_cln_finished, dance_cln_reset),
+    [TD_RCMD_LANG2] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, x_finished, x_reset),
+    //[TD_RCMD_LANG2] = ACTION_TAP_DANCE_DOUBLE(KC_RCMD, KC_LNG2),
+    [TD_Z_SFTZ] = ACTION_TAP_DANCE_DOUBLE(KC_Z, S(KC_Z)),
+    //[TD_LB_RB] = ACTION_TAP_DANCE_DOUBLE(KC_LEFT_BRACKET, KC_RIGHT_BRACKET),
+};
 
 #endif
 
@@ -110,7 +221,7 @@ void oledkit_render_info_user(void) {
 //const uint16_t PROGMEM cb_lang1[] = {KC_LNG2, KC_SPC, COMBO_END};
 
 enum combos {
-    //COMBO_lang1,
+//    COMBO_lang1,
     //COMBO_BTN1,
     //COMBO_BTN2,
     COMBO_COUNT
